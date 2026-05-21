f6311cb4ae0b273a8ee4769c78b97e84363e4715
TurkovaEN <182263781+TurkovaEN@users.noreply.github.com>
2026-05-21 23:45:14 +0600

    Клиент: опция --register для регистрации пользователя на сервере

--- client/src/main.c
+++ client/src/main.c
@@ -7,18 +7,34 @@
 #include <string.h>
 
 #include "../../common/platform.h"
+#include "../../common/net_frame.h"
 #include "client_threads.h"
 
 static void usage(const char* prog) {
-    fprintf(stderr, "Usage: %s <host> <port> <username>\n", prog);
+    fprintf(stderr, "Usage:\n");
+fprintf(stderr, "  %s <host> <port> <username>\n", prog);
+fprintf(stderr, "  %s --register <host> <port> <username>\n", prog);
 }
 
 int main(int argc, char** argv) {
-    if (argc != 4) { usage(argv[0]); return 1; }
-    const char* host = argv[1];
-    int port = atoi(argv[2]);
-    const char* user = argv[3];
+ int do_register = 0;
+ const char* host = NULL;
+ int port = 0;
+ const char* user = NULL;
 
+ if (argc == 4) {
+  host = argv[1];
+  port = atoi(argv[2]);
+  user = argv[3];
+ } else if (argc == 5 && strcmp(argv[1], "--register") == 0) {
+  do_register = 1;
+  host = argv[2];
+  port = atoi(argv[3]);
+  user = argv[4];
+ } else {
+  usage(argv[0]);
+  return 1;
+ }
     if (port <= 0 || port > 65535) { fprintf(stderr, "Bad port\n"); return 1; }
     if (strlen(user) == 0) { fprintf(stderr, "Bad username\n"); return 1; }
 
@@ -54,6 +70,29 @@ int main(int argc, char** argv) {
     }
 
     printf("Connected to %s:%d as %s\n", host, port, user);
+    if (do_register) {
+  char reg_msg[256];
+  snprintf(reg_msg, sizeof(reg_msg), "type=register;user=%s", user);
+  if (send_frame(s, reg_msg, (uint32_t)strlen(reg_msg)) != 0) {
+   fprintf(stderr, "send_frame(register) failed\n");
+   sock_close(s);
+   net_cleanup();
+   return 1;
+  }
+
+  // wait for server response to register
+  char payload[2048 + 1];
+  uint32_t n = 0;
+  int rr = recv_frame(s, payload, 2048, &n);
+  if (rr != 0) {
+   fprintf(stderr, "recv_frame(register) failed\n");
+   sock_close(s);
+   net_cleanup();
+   return 1;
+  }
+  payload[n] = '\0';
+  printf("[server] %s\n", payload);
+ }
 
     // run_client will send login, start recv thread, read commands from stdin
     int rc = run_client(s, user);
